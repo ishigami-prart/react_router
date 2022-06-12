9b8f6dea2597d0684d77f3a43054311712405db5
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-13 08:19:32 +0900

    Queryパラメーター途中

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,7 +1,9 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
       <h1>UrlParameterページです。</h1>
