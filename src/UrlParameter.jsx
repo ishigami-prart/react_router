c9917d76644c9c2208b71f48e363b316b866ccb8
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-14 08:22:52 +0900

    404ページの作成

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -3,11 +3,13 @@ import { useParams, useLocation } from "react-router-dom";
 export const UrlParameter = () => {
   const { id } = useParams();
   const { search } = useLocation();
-  console.log(search);
+  const query = new URLSearchParams(search);
+  console.log(query);
   return (
     <div>
       <h1>UrlParameterページです。</h1>
       <p>パラメーターは{id}です</p>
+      <p>クエリパラメーターは{query.get("name")}です</p>
     </div>
   );
 };
